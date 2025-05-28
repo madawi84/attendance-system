3563f25d42d34ae282ebe3c6f0fa72edafae80f3
Madawi F Alsoyohi <madawi.alsoyohi@hotmail.com>
2025-05-28 07:45:42 +0300

    Register attendance app in INSTALLED_APPS

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -137,3 +137,17 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",        # ← prints our logger.info messages
+    },
+}
